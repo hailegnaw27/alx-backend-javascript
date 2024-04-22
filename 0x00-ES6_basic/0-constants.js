8340649a3de26b25b944a0b040c9058f0fbc19c1
hailegnaw27 <ixvos406@gmail.com>
2024-04-22 14:49:09 -0700

    0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,19 @@
+// Function taskFirst uses const to declare the variable task
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+// Function getLast() returns the string ' is okay'
+export function getLast() {
+  return ' is okay';
+}
+
+// Function taskNext uses let to declare the variable combination
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast(); // Concatenating the return value from getLast()
+
+  return combination;
+}
+
