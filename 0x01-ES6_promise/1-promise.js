92db3a625c8f9f6bf935fd900b4b6124797f5724
hailegnaw27 <ixvos406@gmail.com>
2024-04-24 12:41:54 -0700

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,16 +1,12 @@
 export default function getFullResponseFromAPI(success) {
-  // Return a promise based on the input boolean parameter
   return new Promise((resolve, reject) => {
     if (success) {
-      // Resolve the promise with an object containing status and body
       resolve({
         status: 200,
-        body: 'Success',
+        body: "Success",
       });
     } else {
-      // Reject the promise with an error object containing a message
-      reject(new Error('The fake API is not working currently'));
+      reject(new Error("The fake API is not working currently"));
     }
   });
 }
-
