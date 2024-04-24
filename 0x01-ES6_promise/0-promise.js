00127487c5b4b434595bce1234fd60766d49b84f
hailegnaw27 <ixvos406@gmail.com>
2024-04-24 12:37:06 -0700

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,3 @@
 export default function getResponseFromAPI() {
-  // Return a Promise
-  return new Promise((resolve, reject) => {
-    // Example: Resolve the Promise with a success message
-    resolve('Success');
-    // Optionally, you can reject the Promise if needed
-    // reject('Error');
-  });
+  return new Promise(() => {});
 }
-
