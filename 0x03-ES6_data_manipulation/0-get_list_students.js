fdebece7d15553e703c28312bbc499309e446d00
hailegnaw27 <ixvos406@gmail.com>
2024-04-30 13:14:21 -0700

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,7 @@
-// Define an arrow function named getListStudents
 const getListStudents = () => [
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' },
-];
-
-// Export the function as the default export
-export default getListStudents;
-
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  
+  export default getListStudents;
